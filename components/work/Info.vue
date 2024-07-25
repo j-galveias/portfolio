c9b52d6b3eafffc2b7c7eeae610afec4c83aa68a
j-galveias <brainsuprajr@gmail.com>
2024-07-25 22:12:47 +0100

    Created work page

---
+++ components/work/Info.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+type Props = {
+  company: string
+  role: string
+  startDate: string
+  endDate?: string
+  description: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-5">
+    <div class="flex text-xl justify-between">
+      <strong>{{ company }} - {{ role }}</strong>
+      <span>{{ startDate }} - {{ endDate ?? 'Present' }}</span>
+    </div>
+    <span>{{ description }}</span>
+  </div>
+</template>
--- pages/work.vue
+++ pages/work.vue
@@ -3,5 +3,22 @@
 </script>
 
 <template>
-asd
+  <div class="flex flex-col gap-10 h-full w-full text-white">
+    <span class="text-3xl underline">
+      Work:
+    </span>
+    <WorkInfo
+      company="Tpg"
+      role="Full-Stack Developer"
+      start-date="March 2023"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin posuere eleifend augue, vel tempor orci. Suspendisse nisl ante, feugiat sit amet velit in, ornare mollis urna. Proin at quam id dolor ultricies placerat. Aenean dictum est ex, sit amet sollicitudin est sollicitudin vitae. Ut egestas elit a lacus sodales, quis bibendum urna congue. "
+    />
+    <WorkInfo
+      company="NTT Data"
+      role="AR Developer"
+      start-date="July 2022"
+      end-date="Sept. 2022"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin posuere eleifend augue, vel tempor orci. Suspendisse nisl ante, feugiat sit amet velit in, ornare mollis urna. Proin at quam id dolor ultricies placerat. Aenean dictum est ex, sit amet sollicitudin est sollicitudin vitae. Ut egestas elit a lacus sodales, quis bibendum urna congue. "
+    />
+  </div>
 </template>
